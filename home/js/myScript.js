122e820ade0b670432f9c41f3903668d4a6dfec9
apcedgalauran <edgalauran@student.apc.edu.ph>
2024-12-17 08:39:00 +0800

    updated the other HTML DOM function

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -3,9 +3,9 @@ function myFunction() {
 }
 
 function myFunction2() {
-    const element = document.getElementsByTagName("br");
-
-    document.getElementById("demo").innerHTML = 'The text in first <br> tag (index 0) is: ' + element[0].innerHTML;
-
-
+    const element = document.getElementsByTagName("h1");
+ 
+    document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
+ 
+ 
 }
